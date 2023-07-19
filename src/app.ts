a3d986e4513614c8dd85747b01b759bea654301e
uddinmayraj <mayrajuddin03@gmail.com>
2023-07-19 12:11:27 +0600

    retry

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
@@ -8,10 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-//routes	
+//routes
 app.get('/', (req: Request, res: Response) => {
-    res.send('University Management Service')
+  res.send('University Management Service')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
+import mongoose from 'mongoose'
+import app from './app'
 import config from './config'
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('database connected');
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log("database connection failed", err);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database connected')
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('database connection failed', err)
+  }
 }
 
-main();
+main()
