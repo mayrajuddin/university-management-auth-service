8c54d6952f638a1d59b87e3a754ded34a75d6f6c
mayrajuddin <mayrajuddin03@gmail.com>
2023-10-05 23:18:20 +0600

    controller of student created-[ get, update, delete, getbyId

--- src/app/modules/student/student.Controller.ts
+++ src/app/modules/student/student.Controller.ts
@@ -5,24 +5,56 @@ import httpStatus from 'http-status';
 import pick from '../../../shared/pick';
 import { fielterAbleField } from './studentConstant';
 import { paginationFields } from '../../../constants/paginations';
+import { IStudent } from './student.Interface';
+import { studentService } from './student.Service';
 
 const getAllStudents = catchAsync(async (req: Request, res: Response) => {
   const filter = pick(req.query, fielterAbleField);
   const paginationOptions = pick(req.query, paginationFields);
 
-  //   const result = a;
-  sendResponse(res, {
+  const result = await studentService.getAllStudents(filter, paginationOptions);
+  sendResponse<IStudent[]>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Get all students successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await studentService.getSingleStudent(id);
+  sendResponse<IStudent>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get  student successfully',
     data: result,
   });
 });
-const getSingleStudent = catchAsync(async (req: Request, res: Response) => {});
-
-const deleteStudent = catchAsync(async (req: Request, res: Response) => {});
+const updateStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+  const result = await studentService.updateStudent(id, updatedData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student updated successfully!',
+    data: result,
+  });
+});
+const deleteStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await studentService.deleteStudent(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student deleted successfully!',
+    data: result,
+  });
+});
 export const studentController = {
   getAllStudents,
   getSingleStudent,
+  updateStudent,
   deleteStudent,
 };
---
+++ src/app/modules/student/student.Service.ts
@@ -0,0 +1,10 @@
+const getAllStudents = (filter, pagination) => {};
+const getSingleStudent = (id: string) => {};
+const updateStudent = (id: string, payload) => {};
+const deleteStudent = (id: string) => {};
+export const studentService = {
+  getAllStudents,
+  getSingleStudent,
+  updateStudent,
+  deleteStudent,
+};
